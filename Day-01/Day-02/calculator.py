edc3ae027ed77889c6945f212b54e9d4b559af2c
Aditya Dhanraj <dhanrajaditya2076@gmail.com>
2026-07-24 20:14:58 +0530

    Create calculator.py

---
+++ Day-01/Day-02/calculator.py
@@ -0,0 +1,13 @@
+a=eval(input("enter the value:1-"))
+b=eval(input("enter the value:2-"))
+oper=input ("oper...")
+if oper=="+":
+    print(a+b)
+if oper == "-":
+    print(a-b)
+if oper=="*":
+    print(a*b)
+if oper == "/":
+    print(a/b)
+if oper=="%":
+   print(a%b)
